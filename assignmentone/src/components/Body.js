c9fe114077d57b6341587fc4a5f7c8bd0de88da3
JoelRanjan <joelranjan009@gmail.com>
2024-07-22 11:17:04 +0530

    custom hook

--- assignmentone/src/components/Body.js
+++ assignmentone/src/components/Body.js
@@ -3,18 +3,10 @@ import Sidebar from "./Sidebar";
 import MainContent from "./MainContent";
 import { useDispatch } from "react-redux";
 import { addCustomerData } from "../utils/dataSlice";
+import useGetData from "../utils/useGetData";
 
 const Body = () => {
-  const dispatch = useDispatch();
-  const getData = async () => {
-    const data = await fetch("http://test.api.boxigo.in/sample-data/ ");
-    const jsonData = await data.json();
-    dispatch(addCustomerData(jsonData)); //Customer_Estimate_Flow
-  };
-
-  useEffect(() => {
-    getData();
-  });
+  useGetData();
   return (
     <div className="flex ">
       <div className="">
---
+++ assignmentone/src/utils/useGetData.js
@@ -0,0 +1,18 @@
+import { useDispatch } from "react-redux";
+import { addCustomerData } from "./dataSlice";
+import { useEffect } from "react";
+
+const useGetData = () => {
+  const dispatch = useDispatch();
+  const getData = async () => {
+    const data = await fetch("http://test.api.boxigo.in/sample-data/ ");
+    const jsonData = await data.json();
+    dispatch(addCustomerData(jsonData)); //Customer_Estimate_Flow
+  };
+
+  useEffect(() => {
+    getData();
+  });
+};
+
+export default useGetData;
